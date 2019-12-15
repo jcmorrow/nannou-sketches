732ff9294d0e0e624d62243b3ff7948af860b63c
Josh Morrow <josh@jcmorrow.com>
2019-12-15 00:30:02 -0800

    Sin wave

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,45 @@
+extern crate nannou;
+
 use nannou::prelude::*;
 
 fn main() {
     nannou::app(model).update(update).simple_window(view).run();
 }
 
-struct Model {}
-
-fn model(_app: &App) -> Model {
-    Model {}
+struct Model {
+    points: Vec<Vector2>,
+    time: f32,
 }
 
-fn update(_app: &App, _model: &mut Model, _update: Update) {}
+fn model(_app: &App) -> Model {
+    Model {
+        time: 0.,
+        points: Vec::new(),
+    }
+}
 
-fn view(app: &App, _model: &Model, frame: &Frame) {
-    // Prepare to draw.
+// By default, `update` is called right before `view` is called each frame.
+fn update(app: &App, model: &mut Model, _update: Update) {
+    model.time += 0.1;
+    model.points.push(
+        Vector2::new(
+            model.time as f32 * 10.,
+            (model.time as f64 * 10.0).sin() as f32,
+        ) * 10.0,
+    );
+}
+
+fn view(app: &App, model: &Model, frame: &Frame) {
+    // Begin drawing.
     let draw = app.draw();
+    // Draw dark gray for the background
+    draw.background().color(BLACK);
 
-    // Clear the background to purple.
-    draw.background().color(PLUM);
+    // Let's graph a line maybe?
+    for point in &model.points {
+        draw.ellipse().color(WHITE).w(2.0).h(2.0).xy(point.clone());
+    }
 
-    // Draw a blue ellipse with default size and position.
-    draw.ellipse().color(STEELBLUE);
-
-    // Write to the window frame.
+    // Write the result of our drawing to the window's frame.
     draw.to_frame(app, &frame).unwrap();
 }
